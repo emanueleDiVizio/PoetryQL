8afea64edf95ed53053b70f5907bb74aef12a880
emanueledivizio <emadivizio@gmail.com>
2017-12-09 17:49:11 +0000

    hoc refactor

--- app/src/ui/PaintingBackground/index.js
+++ app/src/ui/PaintingBackground/index.js
@@ -4,4 +4,4 @@ import apollo from '../../config/apollo/hoc';
 import getRandomPainting from './gql/getRandomPaintingQuery.gql';
 import PaintingBackground from './PaintingBackground';
 
-export default apollo(getRandomPainting, PaintingBackground);
+export default apollo({ query: getRandomPainting }, PaintingBackground);
