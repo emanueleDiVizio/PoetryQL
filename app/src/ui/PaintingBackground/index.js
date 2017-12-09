e984cf778e5681a7179ae9fa4aaa4cba5e6b0478
emanueledivizio <emadivizio@gmail.com>
2017-12-09 16:15:11 +0000

    Painting background

---
+++ app/src/ui/PaintingBackground/index.js
@@ -0,0 +1,7 @@
+//  @ flow
+import apollo from '../../config/apollo/hoc';
+
+import getRandomPainting from './gql/getRandomPaintingQuery.gql';
+import PaintingBackground from './PaintingBackground';
+
+export default apollo(getRandomPainting, PaintingBackground);
